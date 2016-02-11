7ce81b42454d672caefda83a1b0705bd6767fc00
benjaminpettus <benjaminpettus@gmail.com>
2016-02-10 16:10:50 -1000

    added pages to views and corresponding get routes

--- server.js
+++ server.js
@@ -5,10 +5,24 @@ var app = express();
 app.set('view engine', 'jade');
 app.set('views', 'views');
 
+
+
+
+//routes
 app.get('/', function (req, res) {
   console.log("NSJJ Club");
   res.render('index', {});
 });
+
+app.get('/schedule', function (req, res) {
+  console.log('schedule');
+  res.render('schedule', {});
+});
+
+app.get('/contact', function (req, res) {
+  console.log('contact');
+  res.render('contact', {});
+}); 
 
 var server = app.listen(3000, function () {
   console.log('server litening on port' , server.address().port);
